da94be419e962b065f16e5fdd16af519928ce9ba
achi113s <latourgiorgio@gmail.com>
2022-08-24 22:31:06 -0400

    starting to add flask

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.uic import loadUiType
 import requests
 import creds
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
 
 def get_weather(zipcode: str) -> dict:
     APICALL = f'https://api.openweathermap.org/data/2.5/weather'
@@ -16,37 +22,7 @@ def get_weather(zipcode: str) -> dict:
         # success getting weather forecast
         return resp.json()
 
-zipcode = '23322'
-weather = get_weather(zipcode)
-temperature = weather['main']['temp']
-print(f'The temperature in {zipcode} is {temperature} degrees Fahrenheit.')
-
-# # file from QtCreator
-# qtcreator_file = '/Users/giorgio/Documents/mainwindow.ui'
-# # import UI file, returns tuple of form class and Qt Base class
-# Ui_MainWindow, QtBaseClass = loadUiType(qtcreator_file)
-
-
-# class App(QMainWindow, Ui_MainWindow):
-#     # constructor
-#     def __init__(self):
-#         # initialize main window class passing the App instance
-#         QMainWindow.__init__(self)
-#         Ui_MainWindow.__init__(self)
-#         self.setupUi(self)
-
-#         # since we named the button object in Qt Creator we can
-#         # reference it here with self
-#         self.getWeather_Button.clicked.connect(self.getWeather_Action)
-
-#     def getWeather_Action(self):
-#         zipcode = str(self.zipcode_Box.text()).strip()
-#         weather = get_weather(zipcode)
-
-
-# if __name__ == '__main__':
-#     app = QApplication([])
-#     window = App()
-#     window.show()
-#     app.exec_()
-#     print("done")
+# zipcode = '23322'
+# weather = get_weather(zipcode)
+# temperature = weather['main']['temp']
+# print(f'The temperature in {zipcode} is {temperature} degrees Fahrenheit.')
